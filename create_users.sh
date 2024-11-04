b7f56881e401236f66918284bcbcc6a077f056c0
Maksim Andreyko <60541760+MaxAndreyko@users.noreply.github.com>
2024-11-04 13:00:57 +0300

    add pretty log printing to create_users.sh script

--- create_users.sh
+++ create_users.sh
@@ -34,17 +34,16 @@ TEAM_USER="team"
 LOCAL_HOST=$(hostname)
 
 # Prompt for the username to create on the remote machine
-read -p "Enter the username to create: " NEW_USER
+read -p "Введите имя пользователя для создания: " NEW_USER
 
 # Prompt for the password for the new user
-read -sp "Enter the password for new user: " PASSWORD
+read -sp "Введите пароль для нового пользователя: " PASSWORD
 echo
 
 # Create a temporary file to store public keys
 TEMP_FILE="/home/$TEAM_USER/.ssh/authorized_keys.temp"
 touch $TEMP_FILE
 > "$TEMP_FILE"  # Clear the file if it exists
-echo "Path to temp file: $TEMP_FILE"
 
 # Read host addresses and hostnames from the file into an array
 HOSTS_FILE="$(dirname "$0")/hosts.txt"
@@ -55,12 +54,13 @@ mapfile -t HOSTS < "$HOSTS_FILE"
 for ENTRY in "${HOSTS[@]}"; do
     # Split entry into HOST and HOSTNAME
     read -r HOST HOSTNAME <<< "$ENTRY"
-    
-    echo "Processing host: $HOST (Hostname: $HOSTNAME)"
+
+    print_header "Создание пользователя $NEW_USER для хоста: $HOSTNAME."
+    echo "ВНИМАНИЕ! При выполнении команд на удаленных хостах потребуется ввод пароля пользователя $TEAM_USER"
 
     # Check if the hostname matches the current hostname
     if [[ "$HOSTNAME" != "$LOCAL_HOST" ]]; then
-        
+
         # Command to create the user and set the password
         SSH_COMMAND="sudo useradd -m -s /bin/bash $NEW_USER && echo '$NEW_USER:$PASSWORD' | sudo chpasswd"
 
@@ -69,43 +69,40 @@ for ENTRY in "${HOSTS[@]}"; do
 
         # Check if the user was created successfully
         if [ $? -eq 0 ]; then
-            echo "User '$NEW_USER' created successfully on $HOST."
+            print_header "Пользователь $NEW_USER создан для хоста: $HOSTNAME."
 
-            echo "Generating public ssh-key on $HOST for $NEW_USER..."
+            print_header "Генерация публичного SSH-ключа на $HOSTNAME для пользователя $NEW_USER..."
             # Generate SSH key pair for the new user on remote machine without logging in via SSH
             SSH_KEY_GEN_COMMAND="sudo -u $NEW_USER ssh-keygen -t rsa -b 4096 -f /home/$NEW_USER/.ssh/id_rsa -N ''"
-            ssh -t "$TEAM_USER@$HOST" "$SSH_KEY_GEN_COMMAND"
+            ssh -t "$TEAM_USER@$HOST" "$SSH_KEY_GEN_COMMAND" || error_exit "Не удалось сгенерировать публичный ключ."
 
             # Copy public key from remote machine to local temporary file
-            echo "Copying public key from '$NEW_USER' on $HOST to local temporary file..."
-            sshpass -p "$PASSWORD" ssh -t "$NEW_USER@$HOST" "cat /home/$NEW_USER/.ssh/id_rsa.pub" >> "$TEMP_FILE"
-            echo "Public key added to $TEMP_FILE."
-
+            echo "Копирование публичного SSH-ключа для пользователя $NEW_USER на $HOSTNAME в временный файл..."
+            sshpass -p "$PASSWORD" ssh -t "$NEW_USER@$HOST" "cat /home/$NEW_USER/.ssh/id_rsa.pub" >> "$TEMP_FILE" || error_exit "Не удалось скопировать ключ."
         else
-            echo "Failed to create user '$NEW_USER' on $HOST."
+            error_exit "Не удалось создать пользователя $NEW_USER на хосте $HOSTNAME."
         fi
 
     else
-        echo "Hostname equals local hostname ($LOCAL_HOST)."
-        sudo useradd -m -s /bin/bash $NEW_USER && echo $NEW_USER:$PASSWORD | sudo chpasswd
-        if [ $? -eq 0 ]; then
-            echo "User $NEW_USER created successfully on $HOST."
 
-            echo "Generating public ssh-key on $HOST for $NEW_USER..."
+        sudo useradd -m -s /bin/bash "$NEW_USER" && echo "$NEW_USER":"$PASSWORD" | sudo chpasswd
+        if [ $? -eq 0 ]; then
+            print_header "Пользователь $NEW_USER создан для хоста: $HOSTNAME."
+
+            print_header "Генерация публичного SSH-ключа на $HOSTNAME для пользователя $NEW_USER..."
             # Generate SSH key pair for the new user on local machine
-            sudo -u $NEW_USER ssh-keygen -t rsa -b 4096 -f /home/$NEW_USER/.ssh/id_rsa -N ""
+            sudo -u "$NEW_USER" ssh-keygen -t rsa -b 4096 -f "/home/$NEW_USER/.ssh/id_rsa" -N "" || error_exit "Не удалось сгенерировать ключ."
 
             # Copy public key to local temporary file
-            echo "Copying public key from '$NEW_USER' on $HOST to local temporary file..."
-            sudo -u $NEW_USER cat /home/$NEW_USER/.ssh/id_rsa.pub >> "$TEMP_FILE"
-            echo "Public key added to $TEMP_FILE."
+            echo "Копирование публичного SSH-ключа для пользователя $NEW_USER на $HOSTNAME в временный файл..."
+            sudo -u "$NEW_USER" cat "/home/$NEW_USER/.ssh/id_rsa.pub" >> "$TEMP_FILE" || error_exit "Не удалось скопировать ключ."
         else
-            echo "Failed to create user '$NEW_USER' on $HOST."
+            error_exit "Не удалось создать пользователя $NEW_USER на хосте $HOSTNAME."
         fi
         continue
     fi
 
-done  # End of loop over hosts
+done
 
 # Iterate over each host entry
 for ENTRY in "${HOSTS[@]}"; do
@@ -113,24 +110,23 @@ for ENTRY in "${HOSTS[@]}"; do
     SERVER_IP=$(echo "$ENTRY" | awk '{print $1}')
     SERVER_HOST=$(echo "$ENTRY" | awk '{print $2}')
 
-    echo "Adding collected public keys to authorized_keys of $SERVER_HOST"
+    print_header "Добавление созданных публичных ключей на $SERVER_HOST ..."
     if [[ "$SERVER_HOST" == "$LOCAL_HOST" ]]; then
-        sudo cp $TEMP_FILE /home/$NEW_USER/.ssh/authorized_keys
-        sudo cat $HOSTS_FILE >> /etc/hosts && sudo sed -i '/^127\.0\.0\.1/ s/^/#/' /etc/hosts && sudo sed -i '/^127\.0\.1\.1/ s/^/#/' /etc/hosts
+        sudo cp "$TEMP_FILE" "/home/$NEW_USER/.ssh/authorized_keys"
+        sudo cat "$HOSTS_FILE" >> /etc/hosts && sudo sed -i '/^127\.0\.0\.1/ s/^/#/' /etc/hosts && sudo sed -i '/^127\.0\.1\.1/ s/^/#/' /etc/hosts
     else
-        sshpass -p "$PASSWORD" scp $TEMP_FILE $NEW_USER@$SERVER_IP:/home/$NEW_USER/.ssh/authorized_keys
+        sshpass -p "$PASSWORD" scp "$TEMP_FILE" "$NEW_USER@$SERVER_IP:/home/$NEW_USER/.ssh/authorized_keys"
 
         # Copy hosts hosts.txt to temp file on remote host
-        echo "Copying hosts.txt to $SERVER_HOST temp ..."
-        scp "$HOSTS_FILE" "$TEAM_USER@$SERVER_IP:/tmp/"
+        print_header "Копирование файла hosts.txt на $SERVER_HOST ..."
+        scp "$HOSTS_FILE" "$TEAM_USER@$SERVER_IP:/tmp/" || error_exit "Не удалось скопировать файл hosts.txt."
 
         # Add all entries from hosts.txt to /etc/hosts on remote machine
-        echo "Adding entries from hosts.txt to /etc/hosts on $SERVER_IP..."
+        print_header "Копирование содержимого файла host.txt в /etc/hosts на $SERVER_HOST ..."
         ADD_AND_EDIT_HOSTS_COMMAND="sudo bash -c 'cat /tmp/$(basename "$HOSTS_FILE") >> /etc/hosts' && sudo sed -i '/^127\.0\.0\.1/ s/^/#/' /etc/hosts && sudo sed -i '/^127\.0\.1\.1/ s/^/#/' /etc/hosts"
-        ssh -t "$TEAM_USER@$SERVER_IP" "$ADD_AND_EDIT_HOSTS_COMMAND"
-        echo "Entries from hosts.txt added to /etc/hosts on $SERVER_IP."
+        ssh -t "$TEAM_USER@$SERVER_IP" "$ADD_AND_EDIT_HOSTS_COMMAND" || error_exit "Не удалось скопировать содержимое."
     fi
 
 done
 
-echo "Connection initialization completed"
+print_header "Завершение автоматического создания подключения по SSH."
